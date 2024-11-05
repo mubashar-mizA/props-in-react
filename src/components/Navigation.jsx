9951028a3f186f61b480657b82bd219a5502cb77
mubashar-mizA <mirzamubashar535@gmail.com>
2024-11-05 12:53:05 +0500

    applying css to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 const Navigation = () => {
     return (
-        <div className="text-red-900 flex max-w-5xl bg-blue-300 mx-auto items-center justify-between p-2 lg:m-1" >
+        <div className=" flex max-w-5xl bg-blue-300 mx-auto items-center justify-between lg:justify-center lg:gap-52 lg:p-4 p-2 lg:my-1" >
             <div>sharpEdg.com</div>
             <div className="flex gap-4">
                 <p>Login</p>
